5639c2357143d1d1d8ea9a9b83c071ed4363d36e
darzu <daryl.zuniga@gmail.com>
2016-07-13 10:21:59 -0700

    updates shims

--- libs/microbit/shims.d.ts
+++ libs/microbit/shims.d.ts
@@ -205,23 +205,6 @@ declare namespace input {
     function onButtonPressed(button: Button, body: () => void): void;
 
     /**
-     * Attaches code to run when the screen is facing up.
-     * @param body TODO
-     */
-    //% help=input/on-gesture weight=84 blockGap=8
-    //% blockId=device_gesture_event block="on |%NAME" icon="\uf135" shim=input::onGesture
-    function onGesture(gesture: Gesture, body: () => void): void;
-
-    /**
-     * Do something when a pin(``P0``, ``P1`` or both ``P2``) is pressed.
-     * @param name TODO
-     * @param body TODO
-     */
-    //% help=input/on-pin-pressed weight=83
-    //% blockId=device_pin_event block="on pin|%NAME|pressed" icon="\uf094" shim=input::onPinPressed
-    function onPinPressed(name: TouchPin, body: () => void): void;
-
-    /**
      * Get the button state (pressed or not) for ``A`` and ``B``.
      */
     //% help=input/button-is-pressed weight=57
@@ -229,53 +212,6 @@ declare namespace input {
     //% blockId=device_get_button2
     //% icon="\uf192" blockGap=8 shim=input::buttonIsPressed
     function buttonIsPressed(button: Button): boolean;
-
-    /**
-     * Get the current compass compass heading in degrees.
-     */
-    //% help=input/compass-heading 
-    //% weight=56 icon="\uf14e"
-    //% blockId=device_heading block="compass heading (°)" blockGap=8 shim=input::compassHeading
-    function compassHeading(): number;
-
-    /**
-     * Gets the temperature in Celsius degrees (°C).
-     */
-    //% weight=55 icon="\uf06d"
-    //% help=input/temperature
-    //% blockId=device_temperature block="temperature (°C)" blockGap=8 shim=input::temperature
-    function temperature(): number;
-
-    /**
-     * Get the acceleration value in milli-gravitys (when the board is laying flat with the screen up, x=0, y=0 and z=-1024)
-     * @param dimension TODO
-     */
-    //% help=input/acceleration weight=54 icon="\uf135"
-    //% blockId=device_acceleration block="acceleration (mg)|%NAME" blockGap=8 shim=input::acceleration
-    function acceleration(dimension: Dimension): number;
-
-    /**
-     * Reads the light level applied to the LED screen in a range from ``0`` (dark) to ``255`` bright.
-     */
-    //% help=input/light-level weight=53
-    //% blockId=device_get_light_level block="light level" blockGap=8 icon="\uf185" shim=input::lightLevel
-    function lightLevel(): number;
-
-    /**
-     * The pitch of the device, rotation along the ``x-axis``, in degrees.
-     * @param kind TODO
-     */
-    //% help=/input/rotation weight=52
-    //% blockId=device_get_rotation block="rotation (°)|%NAME" blockGap=8 icon="\uf197" shim=input::rotation
-    function rotation(kind: Rotation): number;
-
-    /**
-     * Get the magnetic force value in ``micro-Teslas`` (``µT``). This function is not supported in the simulator.
-     * @param dimension TODO
-     */
-    //% help=input/magnetic-force weight=51
-    //% blockId=device_get_magnetic_force block="magnetic force (µT)|%NAME" blockGap=8 icon="\uf076" shim=input::magneticForce
-    function magneticForce(dimension: Dimension): number;
 
     /**
      * Gets the number of milliseconds elapsed since power on.
@@ -289,22 +225,6 @@ declare namespace input {
      */
     //% help=input/calibrate weight=0 shim=input::calibrate
     function calibrate(): void;
-
-    /**
-     * Get the pin state (pressed or not). Requires to hold the ground to close the circuit.
-     * @param name pin used to detect the touch
-     */
-    //% help=input/pin-is-pressed weight=58 block="pin|%NAME|is pressed" icon="\uf094" shim=input::pinIsPressed
-    function pinIsPressed(name: TouchPin): boolean;
-
-    /**
-     * Sets the accelerometer sample range in gravities.
-     * @param range a value describe the maximum strengh of acceleration measured
-     */
-    //% help=input/set-accelerator-range
-    //% blockId=device_set_accelerometer_range block="set accelerometer|range %range" icon="\uf135"
-    //% weight=5 shim=input::setAccelerometerRange
-    function setAccelerometerRange(range: AcceleratorRange): void;
 }
 
 
@@ -442,107 +362,6 @@ declare namespace led {
 
     //% color=351 weight=30
 declare namespace pins {
-
-    /**
-     * Read the specified pin or connector as either 0 or 1
-     * @param name pin to read from
-     */
-    //% help=pins/digital-read-pin weight=30
-    //% blockId=device_get_digital_pin block="digital read|pin %name" blockGap=8 shim=pins::digitalReadPin
-    function digitalReadPin(name: DigitalPin): number;
-
-    /**
-     * Set a pin or connector value to either 0 or 1.
-     * @param name pin to write to
-     * @param value value to set on the pin, 1 eg,0
-     */
-    //% help=pins/digital-write-pin weight=29
-    //% blockId=device_set_digital_pin block="digital write|pin %name|to %value" shim=pins::digitalWritePin
-    function digitalWritePin(name: DigitalPin, value: number): void;
-
-    /**
-     * Read the connector value as analog, that is, as a value comprised between 0 and 1023.
-     * @param name pin to write to
-     */
-    //% help=pins/analog-read-pin weight=25
-    //% blockId=device_get_analog_pin block="analog read|pin %name" blockGap="8" shim=pins::analogReadPin
-    function analogReadPin(name: AnalogPin): number;
-
-    /**
-     * Set the connector value as analog. Value must be comprised between 0 and 1023.
-     * @param name pin name to write to
-     * @param value value to write to the pin between ``0`` and ``1023``. eg:1023,0
-     */
-    //% help=pins/analog-write-pin weight=24
-    //% blockId=device_set_analog_pin block="analog write|pin %name|to %value" blockGap=8 shim=pins::analogWritePin
-    function analogWritePin(name: AnalogPin, value: number): void;
-
-    /**
-     * Configures the Pulse-width modulation (PWM) of the analog output to the given value in **microseconds** or `1/1000` milliseconds.
-     * If this pin is not configured as an analog output (using `analog write pin`), the operation has no effect.
-     * @param name analog pin to set period to
-     * @param micros period in micro seconds. eg:20000
-     */
-    //% help=pins/analog-set-period weight=23 blockGap=8
-    //% blockId=device_set_analog_period block="analog set period|pin %pin|to (µs)%micros" shim=pins::analogSetPeriod
-    function analogSetPeriod(name: AnalogPin, micros: number): void;
-
-    /**
-     * Configures this pin to a digital input, and generates events where the timestamp is the duration that this pin was either ``high`` or ``low``.
-     */
-    //% help=pins/on-pulsed weight=22 blockGap=8
-    //% blockId=pins_on_pulsed block="on|pin %pin|pulsed %pulse" shim=pins::onPulsed
-    function onPulsed(name: DigitalPin, pulse: PulseValue, body: () => void): void;
-
-    /**
-     * Gets the duration of the last pulse in micro-seconds. This function should be called from a ``onPulsed`` handler.
-     */
-    //% help=pins/pulse-micros
-    //% blockId=pins_pulse_duration block="pulse duration (µs)"
-    //% weight=21 shim=pins::pulseDuration
-    function pulseDuration(): number;
-
-    /**
-     * Writes a value to the servo, controlling the shaft accordingly. On a standard servo, this will set the angle of the shaft (in degrees), moving the shaft to that orientation. On a continuous rotation servo, this will set the speed of the servo (with ``0`` being full-speed in one direction, ``180`` being full speed in the other, and a value near ``90`` being no movement).
-     * @param name pin to write to
-     * @param value angle or rotation speed, eg:180,90,0
-     */
-    //% help=pins/servo-write-pin weight=20
-    //% blockId=device_set_servo_pin block="servo write|pin %name|to %value" blockGap=8 shim=pins::servoWritePin
-    function servoWritePin(name: AnalogPin, value: number): void;
-
-    /**
-     * Configures this IO pin as an analog/pwm output, configures the period to be 20 ms, and sets the pulse width, based on the value it is given **microseconds** or `1/1000` milliseconds.
-     * @param name pin name
-     * @param micros pulse duration in micro seconds, eg:1500
-     */
-    //% help=pins/serial-set-pulse weight=19
-    //% blockId=device_set_servo_pulse block="servo set pulse|pin %value|to (µs) %micros" shim=pins::servoSetPulse
-    function servoSetPulse(name: AnalogPin, micros: number): void;
-
-    /**
-     * Sets the pin used when using `pins->analog pitch`.
-     * @param name TODO
-     */
-    //% help=pins/analog-set-pitch weight=12 shim=pins::analogSetPitchPin
-    function analogSetPitchPin(name: AnalogPin): void;
-
-    /**
-     * Emits a Pulse-width modulation (PWM) signal to the current pitch pin. Use `analog set pitch pin` to define the pitch pin.
-     * @param frequency TODO
-     * @param ms TODO
-     */
-    //% help=pins/analog-pitch weight=14 async shim=pins::analogPitch
-    function analogPitch(frequency: number, ms: number): void;
-
-    /**
-     * Configures the pull of this pin.
-     * @param name pin to set the pull mode on
-     * @param pull one of the mbed pull configurations: PullUp, PullDown, PullNone 
-     */
-    //% help=pins/digital-set-pull weight=3
-    //% blockId=device_set_pull block="set pull|pin %pin|to %pull" shim=pins::setPull
-    function setPull(name: DigitalPin, pull: PinPullMode): void;
 
     /**
      * Create a new zero-initialized buffer.
