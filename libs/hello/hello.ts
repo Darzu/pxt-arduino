7460101204df183d445ac182088962c814276957
darzu <daryl.zuniga@gmail.com>
2016-07-15 13:02:29 -0700

    extends sample

--- libs/hello/hello.ts
+++ libs/hello/hello.ts
@@ -5,30 +5,30 @@ basic.plotLeds(`
 # . . . #
 . # # # .
 `);
-// input.onButtonPressed(Button.A, () => {
-//     basic.showLeds(`
-//         . . . . .
-//         # . . . .
-//         # # . . .
-//         # . . . .
-//         . . . . .
-//         `);
-// });
-// input.onButtonPressed(Button.AB, () => {
-//     basic.showLeds(`
-//         . . . . .
-//         . # . # .
-//         . . # . .
-//         . # . # .
-//         . . . . .
-//         `);
-// });
-// input.onButtonPressed(Button.B, () => {
-//     basic.showLeds(`
-//         . . . . .
-//         . . . . #
-//         . . . # #
-//         . . . . #
-//         . . . . .
-//         `);
-// });
+input.onButtonPressed(Button.A, () => {
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # # . . .
+        # . . . .
+        . . . . .
+        `);
+});
+input.onButtonPressed(Button.AB, () => {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `);
+});
+input.onButtonPressed(Button.B, () => {
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . . . #
+        . . . . .
+        `);
+});
